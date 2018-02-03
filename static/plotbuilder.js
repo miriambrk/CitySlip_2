35237f2c0492751acafcda917710d424856b2cb3
nblase22 <nkmoveout@gmail.com>
2018-02-02 19:27:54 -0500

    fixed variables, added labels

--- static/plotbuilder.js
+++ static/plotbuilder.js
@@ -12,27 +12,28 @@ d3.select("#zip_button")
 function checkzip(code) {
     Plotly.d3.json("/zip", function(errr, data){
         
-    
+    poi_pie(38.88, 77.09)
 })};
 
 // POI pie plot taking lat/lng from checkzip funciton
 function poi_pie(lat, lng){
     // call the route with the POI data
-    Plotly.d3.json("/POIdata/" + lat + "/" + lng, function(errr, data){
+    Plotly.d3.json("/POIdata",function(errr, data){
         // create the total number of places of interest and the percentages, place in pie values list
-        var poi_total = data.liquor_store + data.gym + data.park + data.shopping_mall + data.grocery_or_supermarket + data.movie_theater;
-        var ls_pct = (data.liquor_store/poi_total) * 100;
-        var gym_pct = (data.gym/poi_total) * 100;
-        var park_pct = (data.park/poi_total) * 100;
-        var sm_pct = (data.shopping_mall/poi_total) * 100;
-        var gos_pct = (data.grocery_or_supermarket/poi_total) * 100;
-        var mt_pct = (data.movie_theater/poi_total) * 100;
+        console.log(data)
+        var poi_total = data.Liquorstore + data.Gym + data.Park + data.Shoppingmall + data.Groceryorsupermarket + data.Movietheater;
+        var ls_pct = (data.Liquorstore/poi_total) * 100;
+        var gym_pct = (data.Gym/poi_total) * 100;
+        var park_pct = (data.Park/poi_total) * 100;
+        var sm_pct = (data.Shoppingmall/poi_total) * 100;
+        var gos_pct = (data.Groceryorsupermarket/poi_total) * 100;
+        var mt_pct = (data.Movietheater/poi_total) * 100;
         var pie_values = [ls_pct, gym_pct, park_pct, sm_pct, gos_pct, mt_pct];
 
         console.log(pie_values);
 
         // place the individual totals for hover text
-        var ind_totals = [data.liquor_store, data.gym, data.park, data.shopping_mall, data.grocery_or_supermarket, data.movie_theater];
+        var ind_totals = [data.Liquorstore, data.Gym, data.Park, data.Shoppingmall, data.Groceryorsupermarket, data.Movietheater];
         console.log(ind_totals);
 
         // create the pie labels
@@ -43,12 +44,13 @@ function poi_pie(lat, lng){
         for (p=0; p < ind_totals.length; p++){
             poi_desc.push(pie_labels[p] + ": " + ind_totals[p]);
         }
-
+        console.log(poi_desc);
         // create the data part of the pie chart
         var piedata = [{
             values: pie_values,
             labels: pie_labels,
             type:"pie",
+            hoverinfo: "text",
             hovertext: poi_desc
         }];
         
