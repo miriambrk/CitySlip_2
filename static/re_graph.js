ef21854372f0846afa4fdde8b11594f20e0d7bc9
Miriam A Berkowitz <miriambrk@gmail.com>
2018-02-06 20:29:12 -0500

    updated re_graph.js

--- static/re_graph.js
+++ static/re_graph.js
@@ -2,7 +2,8 @@ function build_real_estate_graph(REdata) {
 
 
   //first remove a previously-rendered svg
-  d3.select("svg").remove();
+  d3.select("#homechart").remove();
+  d3.select("#homestooltip").remove();
 
 
   var svgWidth = 600;
@@ -28,6 +29,7 @@ function build_real_estate_graph(REdata) {
   // Create an SVG wrapper, append an SVG group that will hold the chart, and shift the latter by left and top margins.
   var svg = d3.select(".chart")
     .append("svg")
+    .attr("id","homechart")
     .attr("width", svgWidth)
     .attr("height", svgHeight)
     .append("g")
@@ -39,6 +41,7 @@ function build_real_estate_graph(REdata) {
   d3.select(".chart")
     .append("div")
     .attr("class", "tooltip")
+    .attr("id", "homestooltip")
     .style("opacity", 0);
 
     // This function identifies the minimum and maximum values in a column
@@ -64,15 +67,40 @@ function build_real_estate_graph(REdata) {
   var xTimeScale = d3.scaleTime()
     .range([0, width]);
 
+  var x = d3.scaleTime()
+      .range([0, width]);
+
   // Create scale functions
   var yLinearScale = d3.scaleLinear()
     .range([height, 0]);
+  var y = d3.scaleLinear()
+      .range([height, 0]);
+
+
+  var line1 = d3
+        .line()
+        .x(function(data) {
+          return xTimeScale(data.period);
+        })
+        .y(function(data) {
+          return yLinearScale(data.home_value);
+        });
+  var line2 = d3
+        .line()
+        .x(function(data) {
+          return xTimeScale(data.period);
+        })
+        .y(function(data) {
+          return yLinearScale(data.rental);
+        });
 
   // Create axis functions
   var bottomAxis = d3.axisBottom(xTimeScale)
-    // Specify the number of tick marks (approximately).
-    .ticks(16);
-  var leftAxis = d3.axisLeft(yLinearScale);
+    .ticks(18)
+    .tickFormat(d3.timeFormat("%m-%Y"));
+
+  var leftAxis = d3.axisLeft(yLinearScale)
+    .tickFormat(d3.format('$,'));
 
   // Scale the domain
   xTimeScale.domain(d3.extent(REdata, function(data) {
@@ -80,23 +108,41 @@ function build_real_estate_graph(REdata) {
   }));
   yLinearScale.domain([yMin, yMax]);
 
+  // Add the line paths.
+  // Add the line paths.
+  chart.append("path")
+      .data([REdata])
+      .attr("class", "line green")
+      .attr("id","rentline")
+      .style("stroke-opacity", 0)
+      .attr("d", line2);
+  chart.append("path")
+      .data([REdata])
+      .attr("class", "line blue")
+      .attr("id","homeline")
+      .style("stroke-opacity", 0.8)
+      .attr("d", line1);
+
+
   //add the tool tip
   var toolTip = d3.tip()
     .attr("class", "tooltip")
     .offset([0,0])
-
     .html(function(data) {
 
       if (currentAxisLabelY === "home_value") {
-        var yString = data.home_value;
+        var yVal = data.home_value;
+        // //////////////////////////////////d3.format('$,')
       }
       else {
-        var yString = data.rental;
+        var yVal = data.rental;
       };
+      var formatAmount = d3.format('$,');
+      yString = formatAmount(yVal);
 
-      var formatPeriod = d3.timeFormat("%Y-%m");
+      var formatPeriod = d3.timeFormat("%m-%Y");
       xString = formatPeriod(data.period);
-      return (xString + ": $" + yString);
+      return (xString + ": " + yString);
     });
 
   chart.call(toolTip);
@@ -114,7 +160,9 @@ function build_real_estate_graph(REdata) {
       })
       .attr("r", "5")
       .attr("stroke","black")
+      //*************************ADD FUNCTION TO FILL SO IT WILL BE GREEN FOR RENT
       .attr("fill", "blue")
+
 
       //on hover, show the tooltip
       .on("mouseover", function(data) {
@@ -130,7 +178,15 @@ function build_real_estate_graph(REdata) {
   chart.append("g")
     .attr("transform", `translate(0, ${height})`)
     .attr("class","x-axis") //used for transition
-    .call(bottomAxis);
+    .attr("class","axis")
+    //.call(d3.axisBottom(xTimeScale).ticks(16))
+    .call(bottomAxis)
+    .selectAll("text")
+        .style("text-anchor", "end")
+        .attr("dx", "-.8em")
+        .attr("dy", ".35em")
+        .attr("transform", "rotate(-65)")
+
 
   //append y axis
   chart.append("g")
@@ -146,7 +202,7 @@ function build_real_estate_graph(REdata) {
       .attr("y", 0 - margin.left + 30)
       .attr("x", 0 - (height / 1.7))  // make 1.5 smaller to get the axis title to move down a bit
       .attr("dy", "1em")
-      .attr("class", "axis-text active")
+      .attr("class", "axis-text active blue")
       //default
       .attr("data-axis-name", "home_value")
       .text("Home Prices ($)")
@@ -157,7 +213,7 @@ function build_real_estate_graph(REdata) {
       .attr("y", 0 - margin.left + 10)
       .attr("x", 0 - (height / 1.7))  // make 1.5 smaller to get the axis title to move down a bit
       .attr("dy", "1em")
-      .attr("class", "axis-text inactive")
+      .attr("class", "axis-text inactive green")
       .attr("data-axis-name", "rental")
       .text("Monthly Rent ($)")
 
@@ -165,7 +221,7 @@ function build_real_estate_graph(REdata) {
 
   // Append x-axis labels
   chart.append("text")
-    .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.top + 20) + ")") //make 2.5 larger to get title to move left
+    .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.top + 50) + ")") //make 2.5 larger to get title to move left
     .attr("class", "axis-text x-selected")
     //default
     .attr("data-axis-name", "period")
@@ -204,6 +260,7 @@ function build_real_estate_graph(REdata) {
         // Set the domain for the y-axis
         yLinearScale.domain([yMin, yMax]);
 
+
         // Create a transition effect for the y-axis
         svg
           .select(".y-axis")
@@ -217,15 +274,34 @@ function build_real_estate_graph(REdata) {
           d3
             .select(this)
             .transition()
-
-            // .attr("cx", function(data, index) {
-            //   return xTimeScale(data[currentAxisLabelX]);
-            // })
             .attr("cy", function(data, index) {
               return yLinearScale(+data[currentAxisLabelY]);
             })
-            .duration(500);
+            .duration(500)
+
+            .attr("fill", function(data,index) {
+              if (currentAxisLabelY === "home_value") {
+                return "blue";
+              }
+              else {
+                return "green";
+              }
+            })
+
+
         });
+
+        // change the line
+        if (clickedAxis === "home_value") {
+          d3.select("#homeline").style("stroke-opacity",1);
+          d3.select("#rentline").style("stroke-opacity",0);
+          console.log("homevalue selected");
+        }
+        else {
+          d3.select("#homeline").style("stroke-opacity",0);
+          d3.select("#rentline").style("stroke-opacity",1);
+          console.log("rent selected");
+        }
 
         // Change the status of the axes.
         labelChange(clickedSelection);
